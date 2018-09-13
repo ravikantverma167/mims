c9188e7fa5bbb37568d82d2621ef3d9071e08207
Ravikant <ravikant.vema@mindfulsas.com>
2018-09-13 20:25:49 +0530

    mims a to z product

--- index.js
+++ index.js
@@ -3,30 +3,131 @@ const cheerio = require('cheerio');
 var csv = require('fast-csv');
 const fs = require("fs")
 
+var current = {
+    index: 0,
+    continue: false,
+    alphabet: null,
+    pageNo: null,
+    itemNo: null,
+    items: []
+}
+
+var isTest = false;
+
 const rootUrl = 'http://www.mims.com/'
 
-const click = async (selector, page) => {
+const getCurrent = async () => {
+    var stream = fs.createReadStream(`./data/current.csv`);
 
-    return Promise.all([
-        page.waitForNavigation({
-            timeout: 0
-        }),
-        page.click(selector)
-    ]);
+    return new Promise((resolve, reject) => {
+        csv.fromStream(stream, { headers: true })
+            .on("data", (data) => {
+                data = data || {
+                    index: current.index || 0,
+                    continue: false,
+                    alphabet: current.alphabet,
+                    pageNo: current.pageNo,
+                    itemNo: current.itemNo,
+                }
+                current = {
+                    index: parseInt('' + data.index),
+                    continue: data.continue === 'true',
+                    alphabet: data.alphabet,
+                    pageNo: parseInt('' + data.pageNo),
+                    itemNo: parseInt('' + data.itemNo),
+                    items: []
+                }
+                console.log(current)
+            })
+            .on("end", async () => {
+                console.log("done");
+                return resolve(saveCurrent());
+            });
+    })
+}
 
+const saveCurrent = async () => {
+    // current.continue = true;
+    var csvStream = csv.createWriteStream({
+        headers: true
+    });
+
+    let writableStream = fs.createWriteStream(`./data/current.csv`)
+
+    writableStream.on("finish", function () {
+        console.log("DONE!");
+    });
+
+    csvStream.pipe(writableStream);
+    csvStream.write({
+        index: current.index,
+        continue: true,
+        alphabet: current.alphabet,
+        pageNo: current.pageNo,
+        itemNo: current.itemNo
+    });
+}
+
+const addItem = async (item) => {
+    current.items.push(item);
+    if (current.items.length >= 45) {
+        await saveData()
+    }
+}
+
+const saveData = async () => {
+    let fileName = `${current.alphabet}-${current.pageNo}-${current.itemNo}`;
+    console.log(`writing ${current.items.length} items to file ${fileName}.csv`)
+    var csvStream = csv.createWriteStream({
+        headers: true
+    });
+
+    let writableStream = fs.createWriteStream(`./data/${fileName}.csv`)
+
+    writableStream.on("finish", function () {
+        console.log("DONE!");
+    });
+
+    csvStream.pipe(writableStream);
+    current.items.forEach(item => {
+        csvStream.write(item);
+    })
+    current.items = [];
+
+    await saveCurrent();
+}
+
+const click = async (selector, page, waitFor) => {
+    // console.log("click")
+    await page.click(selector)
+    // .then((data) => {
+    //     return page.waitForNavigation({
+    //         timeout: 0
+    //     })
+    // })
+    waitFor = waitFor || '#wrapper'
+    return page.waitFor(waitFor)
 }
 
 const open = async (url, page) => {
-    return Promise.all[
-        page.goto(url, {
-            timeout: 0
-        })
-        // page.waitForNavigation({
-        //     timeout: 0
-        // })
-    ]
-}
+    // console.log("open")
 
+    await page.goto(url, {
+        timeout: 0
+    })
+    // page.waitForNavigation({
+    //     timeout: 0
+    // })
+
+    return page.waitFor('#wrapper')
+
+    // return new Promise((resolve, reject) => {
+    //     page.on('load', () => {
+    //         console.log("open resolve")
+    //         return resolve()
+    //     });
+    // })
+}
 
 const set = async (selector, value, page) => {
     const name = await page.waitForSelector(selector);
@@ -35,6 +136,7 @@ const set = async (selector, value, page) => {
 }
 
 const login = async (page) => {
+    // console.log("login")
     await page.setViewport({
         width: 1520,
         height: 926
@@ -42,143 +144,162 @@ const login = async (page) => {
     await page.goto('http://www.mims.com/india', {
         timeout: 0
     });
-   // await open('http://www.mims.com/india', page);
-    await click('#authsection > div.signinlink > a', page)
+    // await open('http://www.mims.com/india', page);
+    await click('#authsection > div.signinlink > a', page, "#divOuterContainer")
     await set('#EmailAddress', 'isha131722@gmail.com', page)
     await set('#Password', 'Isha@123', page)
     await click('#btnSubmit', page)
-    console.log('logged in')
+    // console.log('logged in')
 }
 
-const saveData = async (items) => {
-    var csvStream = csv.createWriteStream({
-        headers: true
-    })
-    let writableStream = fs.createWriteStream("./Drugs_Database/A.csv")
+const getText = async (page, selector) => {
+    try {
+        let val = await page.evaluate((sel) => {
+            let el = document.querySelector(sel)
 
-    writableStream.on("finish", function () {
-        console.log("DONE!");
-    });
+            if (el) {
+                return el.innerText || ''
+            }
+            return '';
+        }, selector)
 
-    csvStream.pipe(writableStream);
-    items.forEach(item => {
-        csvStream.write(item);
-    })
-}
 
-const getText = async (selector, $) =>{
-    return $(valueSelector).text()
-}
+        val = val.replace(/,/gi, '|')
+        val = val.replace(/"/gi, '')
 
-const extractValue = async(keySelector, valueSelector) =>{
-    // TODO if data does not exist
-    if(false) {
-        return null
+        return val
     }
-    return {
-        key:getText(keySelector, $),
-        value:  getText(valueSelector, $)
+    catch (err) {
+        return ''
     }
 }
 
-const extractItemFromPage = async (content) => {
-    let $ = cheerio.load(content)
+const extractItemFromPage = async (page) => {
+    await page.waitFor('[itemprop="name"]')
+
     let item = {
-        name: getText('#tdMonoTop > div:nth-child(2) > h1 > span:nth-child(2)', $)
-    };
-    for(let rowNo = 1; rowNo < 5; rowNo++) {
-        let data = extractValue(
-            `#tbMonoCenter > tbody > tr:nth-child(${rowNo}) > td.outline > span`
-            `#tbMonoCenter > tbody > tr:nth-child(${rowNo}) > td.outline-all > span > a > span > span`,
-            $
-        )
-
-        if(data === null) {
-            break;
-        }
-
-        switch(data.key) {
-            case 'Manufacturer': 
-            item.manufacturer = data.value
-            break
-        }
+        row: `${current.pageNo}-${current.itemNo}`
     }
-
-    return item
-
-    // let name_selector = '#tdMonoTop > div:nth-child(2) > h1 > span:nth-child(2)'
-    // name = $(name_selector)
-
-    // data_selactor = '#tbMonoCenter > tbody > tr > td.outline-all > span'
-    // let data = $(data_selactor)
-
-    // keySelector = '#tbMonoCenter > tbody > tr > td.outline > span'
-    // let key = $(keySelector)
-    // if (key === 'Manufacturer') {
-    //     manufacturer = $(data[0]).text()
-    // }
-    // Active_Ingredients = $(data[1]).text(),
-    //     manufacturer = $(data[0]).text(),
-    //     Administration / Uses = $(data[0]).text()
-    // console.log(Active_Ingredients, manufacturer);
-    // return {
-    //     Active_Ingredients: $(data[1]).text(),
-    //     manufacturer: $(data[0]).text()
-    // }
+    try {
+        item.name = await getText(page, '[itemprop="name"]')
+        item.manufacturer = await getText(page, '[itemprop="manufacturer"]')
+        item.composition = await getText(page, '[itemprop="nonProprietaryName"]')
+        item.atc = await getText(page, '#tbMonoCenter > tbody > tr:nth-child(6) > td.outline-all > span')
+        item.administration = await getText(page, '#tbMonoCenter > tbody > tr:nth-child(3) > td.outline-all > span')
+        item.form = await getText(page, '#tdMonoPP > table > tbody > tr:nth-child(2) > td:nth-child(1) > font > strong')
+        item.mrp = await getText(page, '#tdMonoPP > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td > font > strong')
+        return item
+    } catch (err) {
+        return item
+    }
 }
 
 const getDataFromDetailPage = async (selector, page) => {
-    await click(selector, page)
-    let content = await page.content();
-    return extractItemFromPage(content)
+    try {
+        await click(selector, page, "#Head1")
+        return extractItemFromPage(page)
+    } catch (err) {
+        console.log(err)
+        return {
+            row: `${current.pageNo}-${current.itemNo}`
+        }
+    }
 }
 
 const getFromPage = async (content, page) => {
-    let selectors = []
-    let items = []
-    // TODO review 10
-    for (let rowNo = 1; rowNo <= 40; rowNo++) {
-        selectors.push(`#content > table:nth-child(6) > tbody > tr:nth-child(${rowNo + 2}) > td:nth-child(2) > a`)
+    let count = 0;
+    let rowNo = current.itemNo;
+    for (; ; rowNo++) {
+        current.itemNo = rowNo
+        let selector = `#content > table:nth-child(6) > tbody > tr:nth-child(${rowNo + 2}) > td:nth-child(2) > a`
+
+        let exists = await page.evaluate((sel) => document.querySelector(sel), selector)
+        if (!exists) {
+            break;
+        }
+        count++;
+        let item = await getDataFromDetailPage(selector, page)
+
+        // if (item.row = 11) {
+        //     throw new Error('crashed');
+        // }
+        await addItem(item)
+        if (isTest) {
+            break;
+        }
+        await page.goBack();
+        await page.waitFor('#wrapper')
     }
 
-    for (let selector of selectors) {
-        items.push(await getDataFromDetailPage(selector, page))
-    }
-    return items;
+    return count;
 }
-
-const doesNextPageExist = async (content) => {
-    // TODO - 3
-    return true;
-}
-
 
 const getFromAlpahbet = async (char, no, page) => {
+    current.pageNo = no
+    current.itemNo = 1
     await open(`${rootUrl}/india/browse/alphabet/${char}?cat=drug&tab=brand&page=${no}`, page)
+
     let content = await page.content();
 
-    let items = await getFromPage(content, page);
-    let nextPageExist = await doesNextPageExist(content)
-    if (nextPageExist) {
-        let nextData = await getFromAlpahbet(char, no++, page)
+    let noOfItemsFetched = await getFromPage(content, page);
 
-        nextData.forEach(item => {
-            items.push(item)
-        })
+    console.log(`char ${char} page: ${no} count: ${noOfItemsFetched}  `);
+
+    if (isTest) {
+        return;
     }
-    return items
+
+    if (!noOfItemsFetched) {
+        return
+    }
+
+    return getFromAlpahbet(char, no + 1, page)
+}
+
+const extractAlpha = async (page) => {
+    try {
+        await getFromAlpahbet(current.alphabet, current.pageNo, page);
+    } catch (ex) {
+        console.log(ex)
+        await saveData()
+        extractAlpha(page)
+    }
 }
 
 (async () => {
+    await getCurrent();
+
     const browser = await puppeteer.launch({
         headless: false,
         //  executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
     });
     const page = await browser.newPage();
     await login(page);
-    let data = await getFromAlpahbet('y', 3, page);
-    console.log(data)
 
-    await saveData(data);
+    try {
+
+        let alphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        for (let index = current.index; index < alphas.length; index++) {
+            current.index = index;
+            current.alphabet = alphas[index];
+            if (current.continue) {
+                current.continue = false
+            } else {
+                current.pageNo = 1;
+            }
+
+            await extractAlpha(page)
+        }
+
+        if (current.items.length != 0) {
+            await saveData()
+        }
+    } catch (err) {
+        console.log(err)
+        saveCurrent()
+    }
+
     await browser.close();
 })();
+
+
